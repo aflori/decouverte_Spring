f1a59650556ae654a60494b4991d083f98802643
aflori <aurelien.flori@le-campus-numerique.fr>
2023-09-04 13:44:46 +0200

    initial commit (generated code)

---
+++ src/main/java/fr/campus/discoverySpring/campus/helloworld/Main.java
@@ -0,0 +1,13 @@
+package fr.campus.discoverySpring.campus.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
---
+++ src/test/java/fr/campus/discoverySpring/campus/helloworld/springLearning/MainTests.java
@@ -0,0 +1,13 @@
+package fr.campus.discoverySpring.campus.helloworld.springLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
